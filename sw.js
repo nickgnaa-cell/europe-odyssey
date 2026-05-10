1b306d249cf22b7731b8fbd7877ae48502535c34
nickgnaa-cell <nickgnaa@gmail.com>
2026-05-10 20:40:35 +0930

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,29 @@
-const CACHE_NAME = 'europe-odyssey-v1';
-const CORE = ['./', './index.html', './manifest.webmanifest'];
+const CACHE_NAME = 'european-odyssey-v2026-05-10';
+const APP_VERSION = '2026-05-10';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(CORE))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  const url = new URL(event.request.url);
-
-  if (url.origin !== location.origin) {
-    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-    return;
-  }
-
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-      return fetch(event.request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match('./index.html'));
-    })
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
