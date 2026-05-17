f6cc369a7096d43f57e9e2457766b78f7fc601f9
nickgnaa-cell <nickgnaa@gmail.com>
2026-05-17 17:51:25 +0930

    Add files via upload

--- sw.js
+++ sw.js
@@ -69,7 +69,100 @@ self.addEventListener("fetch", event => {
 
   // CACHE FIRST for images/assets
   event.respondWith(
+    caches.match(event.request).const CACHE_NAME = "european-odyssey-2026-v5";
+
+const STATIC_ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch Strategy
+self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") return;
+
+  const requestURL = new URL(event.request.url);
+
+  // NETWORK FIRST for HTML pages
+  if (
+    requestURL.pathname.endsWith(".html") ||
+    requestURL.pathname === "/" ||
+    requestURL.pathname.endsWith("/europe-odyssey/")
+  ) {
+
+    event.respondWith(
+      fetch(event.request)
+        .then(networkResponse => {
+
+          const responseClone = networkResponse.clone();
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+
+          return networkResponse;
+
+        })
+        .catch(() => caches.match(event.request))
+    );
+
+    return;
+  }
+
+  // CACHE FIRST for images/assets
+  event.respondWith(
     caches.match(event.request).then(cachedResponse => {
+
+      return (
+        cachedResponse ||
+
+        fetch(event.request).then(networkResponse => {
+
+          const responseClone = networkResponse.clone();
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+
+          return networkResponse;
+
+        })
+
+      );
+
+    })
+  );
+
+});(cachedResponse => {
 
       return (
         cachedResponse ||
